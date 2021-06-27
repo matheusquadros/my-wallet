bf22c4becc32bba3616a44300f79db6b6bbd2d51
Matheus de Quadros <matheus.quadros@catolicasc.org.br>
2021-06-26 21:57:55 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,143 @@
 import React from 'react';
 import './App.css';
 
+/* http://recharts.org/en-US/examples/CustomActiveShapePieChart */
+
 function App() {
   return (
     <div className="App">
-      <div className="menu-container">
-        menu
-      </div>
-      <nav className="content-container">
-        <header>
-          header
-        </header>
-        <div className="content-wrapper">
-          <div className="wallet-item">
-            <div className="title">test1</div>
-            <div className="gauge">
-              
-            </div>
-            </div>
-          <div className="wallet-item">
-            <div className="title">test2</div>
-            </div>
-          <div className="wallet-item">
-            <div className="title">test2</div>
-            </div>
-        </div>
-      </nav>
 
+      <div className="header">
+        test
+      </div>
+
+      <div className="content-container">
+
+        <div className="goal">
+          <div className="goal-header">
+            <span>Aposentadoria</span>
+          </div>
+          <div className="goal-body">
+            <div className="total-saved">
+              <span className="description">
+                Valor aportado
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+            <div className="total">
+              <span className="description">
+                Valor atual
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+          </div>
+          <div className="goal-footer">
+            <div className="total-applied">
+              <span className="description">
+                Recursos alocados
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+            <div className="not-applied">
+              <span className="description">
+                Não alocado
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="goal">
+          <div className="goal-header">
+            <span>Aposentadoria</span>
+          </div>
+          <div className="goal-body">
+            <div className="total-saved">
+              <span className="description">
+                Valor aportado
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+            <div className="total">
+              <span className="description">
+                Valor atual
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+          </div>
+          <div className="goal-footer">
+            <div className="total-applied">
+              <span className="description">
+                Recursos alocados
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+            <div className="not-applied">
+              <span className="description">
+                Não alocado
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="goal">
+          <div className="goal-header">
+            <span>Aposentadoria</span>
+          </div>
+          <div className="goal-body">
+            <div className="total-saved">
+              <span className="description">
+                Valor aportado
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+            <div className="total">
+              <span className="description">
+                Valor atual
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+          </div>
+          <div className="goal-footer">
+            <div className="total-applied">
+              <span className="description">
+                Recursos alocados
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+            <div className="not-applied">
+              <span className="description">
+                Não alocado
+              </span>
+              <span className="value">
+                R$5,126,00
+              </span>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
