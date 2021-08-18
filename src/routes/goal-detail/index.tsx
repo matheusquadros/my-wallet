e0220f3a79c5627d7cb0ecf2b275a2fc2cf6ee16
Matheus de Quadros <matheus.quadros@catolicasc.org.br>
2021-08-18 19:42:04 -0300

    .

--- src/routes/goal-detail/index.tsx
+++ src/routes/goal-detail/index.tsx
@@ -1,12 +1,135 @@
 import { useState } from 'react';
+
+import { Line } from "react-chartjs-2";
+
 import './index.css';
+
 
 function GoalDetail() {
 
 
+  const defaultOptions = {
+    significantDigits: 2,
+    thousandsSeparator: '.',
+    decimalSeparator: ',',
+    symbol: 'R$'
+  }
+
+  const currencyFormatter = (value: number) => {
+    if (typeof value !== 'number') value = 0.0
+    let options = defaultOptions
+
+    let valueFormatted: string = value.toFixed(options.significantDigits)
+
+    const [currency, decimal] = valueFormatted.split('.')
+    return `${options.symbol} ${currency.replace(
+      /\B(?=(\d{3})+(?!\d))/g,
+      options.thousandsSeparator
+    )}${options.decimalSeparator}${decimal}`
+  }
+
   return (
-    <div>
-      page 2
+    <div className="goal-container">
+      <div className="overall-data">
+        <span className="title">
+          Aposentadoria
+        </span>
+        <div className="data">
+          <div className="overall-content-data">
+
+            <div className="total-saved">
+              <span className="description">
+                Valor aportado
+              </span>
+              <span className="value">
+                {currencyFormatter(123)}
+              </span>
+            </div>
+
+          </div>
+          <div className="overall-content-data">
+            <div className="total">
+              <span className="description">
+                Valor aportado
+              </span>
+              <span className="value">
+                {currencyFormatter(123)}
+              </span>
+            </div>
+          </div>
+          <div className="overall-content-data">
+            <div className="total">
+              <span className="description">
+                Porcentagem alocada
+              </span>
+              <span className="value">
+                80%
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content">
+        <div>
+          <span className="title">Graficos</span>
+          <div className="card chart-container">
+            <Line type={Line} data={{
+              labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+              datasets: [
+                {
+                  label: "First dataset",
+                  data: [33, 43, 53, 63, 83, 93],
+                  fill: true,
+                  backgroundColor: "rgba(27, 141, 217,0.2)",
+                  borderColor: "rgba(27, 141, 217,1)"
+                },
+                {
+                  label: "Second dataset",
+                  data: [33, 43, 53, 63, 84, 95],
+                  fill: false,
+                  borderColor: "#459638"
+                }
+              ]
+            }}
+              options={{
+                plugins: {
+                  legend: false,
+                }
+              }} />
+
+          </div>
+        </div>
+        <div>
+          <span className="title">Registros</span>
+          <div className="card register-log">
+
+            <div className="register-item">
+              <span className="added-date">4 Jun. 2021</span>
+              <div className="register-value">
+                <span className="description">Aporte</span>
+                <span className="value">R$ 15.826,00</span>
+              </div>
+            </div>
+            <div className="register-item">
+              <span className="added-date">4 Jun. 2021</span>
+              <div className="register-value">
+                <span className="description">Aporte</span>
+                <span className="value">R$ 15.826,00</span>
+              </div>
+            </div>
+            <div className="register-item">
+              <span className="added-date">4 Jun. 2021</span>
+              <div className="register-value">
+                <span className="description">Aporte</span>
+                <span className="value">R$ 15.826,00</span>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/routes/home/goals/GoalCard.tsx
+++ src/routes/home/goals/GoalCard.tsx
@@ -14,7 +14,7 @@ const defaultOptions = {
 const currencyFormatter = (value: number) => {
   if (typeof value !== 'number') value = 0.0
   let options = defaultOptions
-  
+
   let valueFormatted: string = value.toFixed(options.significantDigits)
 
   const [currency, decimal] = valueFormatted.split('.')
@@ -29,49 +29,49 @@ interface IProps {
 }
 
 
-const GoalCard : FC<IProps > = ({ goal }: IProps) => {
+const GoalCard: FC<IProps> = ({ goal }: IProps) => {
   return (
-        <div className="goal">
-          <div className="goal-header">
-            <span>{goal.description}</span>
-          </div>
-          <div className="goal-body">
-            <div className="total-saved">
-              <span className="description">
-                Valor aportado
-              </span>
-              <span className="value">
-                {currencyFormatter(goal.addedAmount)}
-              </span>
-            </div>
-            <div className="total">
-              <span className="description">
-                Valor atual
-              </span>
-              <span className="value">
-                {currencyFormatter(goal.currentAmount)}
-              </span>
-            </div>
-          </div>
-          <div className="goal-footer">
-            <div className="total-applied">
-              <span className="description">
-                Recursos alocados
-              </span>
-              <span className="value">
-                {currencyFormatter(goal.allocatedAmount)}
-              </span>
-            </div>
-            <div className="not-applied">
-              <span className="description">
-                Não alocado
-              </span>
-              <span className="value">
-                {currencyFormatter(goal.nonAllocatedAmount)}
-              </span>
-            </div>
-          </div>
-        </div>);
+    <div className="goal">
+      <div className="goal-header">
+        <span>{goal.description}</span>
+      </div>
+      <div className="goal-body">
+        <div className="total-saved">
+          <span className="description">
+            Valor aportado
+          </span>
+          <span className="value">
+            {currencyFormatter(goal.addedAmount)}
+          </span>
+        </div>
+        <div className="total">
+          <span className="description">
+            Valor atual
+          </span>
+          <span className="value">
+            {currencyFormatter(goal.currentAmount)}
+          </span>
+        </div>
+      </div>
+      <div className="goal-footer">
+        <div className="total-applied">
+          <span className="description">
+            Recursos alocados
+          </span>
+          <span className="value">
+            {currencyFormatter(goal.allocatedAmount)}
+          </span>
+        </div>
+        <div className="not-applied">
+          <span className="description">
+            Não alocado
+          </span>
+          <span className="value">
+            {currencyFormatter(goal.nonAllocatedAmount)}
+          </span>
+        </div>
+      </div>
+    </div>);
 }
 
 export default GoalCard;
